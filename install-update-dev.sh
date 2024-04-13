08a4daecd9578e14a9afae75863cbbdfe2722dfd
Hans Løken <hans@ebamse.net>
2024-04-13 12:24:32 +0200

    created brew setup script

---
+++ install-update-dev.sh
@@ -0,0 +1,9 @@
+brew install git
+brew install --cask rectangle
+brew install --cask inkscape
+brew install --cask yt-music
+brew install --cask iterm2
+brew install --cask slack
+brew install --cask flycut
+
+brew upgrade
